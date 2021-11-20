4cbb9a698635489f1f9d5181f05f2fc80dfe48fd
Krzysztof Plonka <krzysztof.plonka@aptiv.com>
2021-11-20 15:42:22 +0100

    LiveTemplates for Django settings

---
+++ settings/mongo.py
@@ -0,0 +1,2 @@
+import mongoengine
+mongoengine.connect(db="$db_name$", host="$db_host$", username="$db_username$", password="$password$")
---
+++ settings/postgres.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "$db_name$",
+        "USER": "$db_username$",
+        "PASSWORD": "$password$",
+        "HOST": "$db_host$",
+        "PORT": 5432,
+    }
+}
